8c6e470766a99b319acf76a7a3218affb6df6ef6
Phil Hanna <ph1204@gmail.com>
2022-12-20 16:47:41 -0500

    Converted unit tests to pytest

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,17 +1,14 @@
-from unittest import TestCase
-
 from semver import Version
 
 
-class TestVersion(TestCase):
+def test_str():
+    expected = "1.2.3-abc+def"
+    v = Version(expected)
+    actual = str(v)
+    assert actual == expected
 
-    def test_str(self):
-        expected = "1.2.3-abc+def"
-        v = Version(expected)
-        actual = str(v)
-        self.assertEqual(expected, actual)
 
-    def test_hash(self):
-        expected = "1.2.3-abc+def"
-        v = Version(expected)
-        self.assertNotEqual(0, hash(v))
+def test_hash():
+    expected = "1.2.3-abc+def"
+    v = Version(expected)
+    assert hash(v) != 0
